6b3cd26a0c4186327c6e33fb7fe943cd814f0a80
Aksha <akshaali01234@gmail.com>
2023-02-07 20:14:29 +0530

    difference of saga and thunk

---
+++ src/Redux/index.js
@@ -0,0 +1,40 @@
+/*
+
+        REDUX-SAGA                                               REDUX-THUNK
+    
+    1. It's a third party middleware library,                  1. Default middleware in redux
+    redux-saga which means we have to integrate 
+    it into our project
+
+    2. Boilerplate work                                        2. We need no boilerplate work
+
+    3. We use generator function.                              3. Promises to perform asyn task  
+
+    4. Here actions clean js function                          4. Here we have to put the async function into
+                                                                  the actioncreater itself
+
+    5. It is preferred in bigger projects                      5. Small and mid level projects
+
+    6. Easy to test, as we have async task in                  6. A difficult to test the async function.
+    another file .
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
